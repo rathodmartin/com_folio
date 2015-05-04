dd5a742cf69d66c4e0ad2d6d624590270a8b4ddb
Rakesh D. Rathod <rakesh@rathodmartin.com>
2015-05-04 18:07:10 -0400

    Modified files to correct errors and bring it up to Book, Chapter 4 standard.

--- admin/controller.php
+++ admin/controller.php
@@ -7,16 +7,16 @@ class FolioController extends JControllerLegacy
 	
 	public function display($cachable = false, $urlparams = false)
 	{
-	/*	require_once JPATH_COMPONENT.'/helpers/folio.php'; */
+		require_once JPATH_COMPONENT.'/helpers/folio.php'; 
 		
 		$view	= $this->input->get('view', 'folios');
 		$layout	= $this->input->get('layout', 'default');
 		$id		= $this->input->getInt('id');
 		
-		if ($view == 'folio' && $layout == 'edit' && !$this->checkEditId('com_folio.edit.foilo', $id))
+		if ($view == 'folio' && $layout == 'edit' && !$this->checkEditId('com_folio.edit.folio', $id))
 		{
 			$this->setError(JText::sprintf('JLIB_APPLICATION_ERROR_UNHELD_ID', $id));
-			$this-setMessage($this->getError(), 'error');
+			$this->setMessage($this->getError(), 'error');
 			$this->setRedirect(JRoute::_('index.php?option=com_folio&view=folios', false));
 			
 			return false;
